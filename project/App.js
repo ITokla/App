edabdd0777d7345560c65b2aa30cc6ea4162c0ea
ITokla <61913925+ITokla@users.noreply.github.com>
2020-07-02 13:06:17 +0800

    Update App.js

--- project/App.js
+++ project/App.js
@@ -1,13 +1,45 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Button } from 'react-native';
+import Constants from 'expo-constants';
+//Drawer navigation
+import { createDrawerNavigator } from '@react-navigation/drawer';
+import { NavigationContainer } from '@react-navigation/native';
+//Drawer navigation
+
+function HomeScreen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+		 //testing
+      <Button
+        onPress={() => navigation.navigate('Personal_Data')}
+        title="Go to Personal_Data"
+      />
+		//testing
+    </View>
+  );
+}
+
+function Personal_Data_Screen({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+		//testing
+      <Button onPress={() => navigation.goBack()} title="Go back home" />
+		//testing
+    </View>
+  );
+}
+
+const Drawer = createDrawerNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Testing!Testing?</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Drawer.Navigator initialRouteName="Home">
+        <Drawer.Screen name="Home" component={HomeScreen} />
+        <Drawer.Screen name="Personal_Data" component={Personal_Data_Screen} />
+      </Drawer.Navigator>
+    </NavigationContainer>
   );
 }
 
